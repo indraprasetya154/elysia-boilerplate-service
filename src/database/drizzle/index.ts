64c229a9ba72f5f9688684cb5418a8b019123be1
Indra Prasetya <indraprasetya154@gmail.com>
2024-04-24 22:19:23 +0700

    feat(db): run drizzle as function

--- src/database/drizzle/index.ts
+++ src/database/drizzle/index.ts
@@ -2,7 +2,16 @@ import { neon } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-http'
 import config from '../../config/config'
 
-const sql = neon(config.db.url!)
-const db = drizzle(sql)
+const dbConnection = async () => {
+    try {
+        const sql = neon(config.db.url!)
+        const db = drizzle(sql)
 
-export default db
+        console.log('💚 Drizzle orm connected to neon serverless')
+        return db
+    } catch (error) {
+        console.log('error connection db')
+    }
+}
+
+export default dbConnection
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import config from './config/config'
 import http from './transports/http'
 import talents from './modules/talents'
+import dbConnection from './database/drizzle'
 
 const main = async () => {
+    dbConnection()
     const app = http
         .get('/', () => 'Hello Elysia')
         .use(talents)
