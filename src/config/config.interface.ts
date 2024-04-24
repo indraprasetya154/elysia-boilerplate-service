11e38e4b9ee4eb75804f48f5e342b2d3e101ebb5
Indra Prasetya <indraprasetya154@gmail.com>
2024-04-24 20:47:12 +0700

    init(config): add initial config

---
+++ src/config/config.interface.ts
@@ -0,0 +1,15 @@
+export interface Config {
+    app: {
+        name: string
+        env: string
+        port: number,
+        log: string
+    },
+    db: {
+        host: string,
+        database: string,
+        username: string,
+        password: string,
+        url: string,
+    }
+}
---
+++ src/config/config.ts
@@ -0,0 +1,22 @@
+import { Config } from "./config.interface";
+import configSchema from "./config.schema"
+
+const env = configSchema
+
+const config: Config = {
+    app: {
+        name: env.APP_NAME,
+        env: env.APP_ENV,
+        log: env.APP_LOG,
+        port: env.APP_PORT_HTTP,
+    },
+    db: {
+        host: env.PG_HOST,
+        username: env.PG_USERNAME,
+        password: env.PG_PASSWORD,
+        database: env.PG_DATABASE,
+        url: env.PG_DATABASE_URL
+    }
+}
+
+export default config
